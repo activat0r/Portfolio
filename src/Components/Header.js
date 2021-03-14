cebf1bee2470c7c720d5b237a0283202346cdcc8
Aditya <adisawant100.as@gmail.com>
2021-03-14 05:59:45 +0530

    Fixed the flickering when zoomed on phones

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,7 +9,7 @@ import { makeStyles, withStyles } from '@material-ui/core/styles';
 import { Button, ButtonGroup } from "@material-ui/core";
 import { palette } from "@material-ui/system";
 import { green, purple, blue } from '@material-ui/core/colors';
-
+import "../CSS/Header.css"
 
 
 const useStyles = makeStyles((theme) => ({
@@ -78,12 +78,10 @@ function Header() {
     },[activeId])
 
 
-
-
     return (
 <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"  data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="true" aria-label="Toggle navigation">
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="true" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
