eb743f917c67b4e0daed4d42dcd625820a901eaf
Aditya <adisawant100.as@gmail.com>
2021-03-16 05:06:20 +0530

    Arranged the buttons linking to repository in the Projects page

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -7,6 +7,35 @@ import CardContent  from "@material-ui/core/CardContent";
 import './../../CSS/Projects.css'
 import { bottom } from "@popperjs/core";
 
+
+function ProjectList({projectList}){
+  return(    
+  <div className="container vertical-align">
+  <div className="row ">
+
+  {projectList.map(projectList => (
+  <div key={projectList.id} className="col-sm-12 col-md-4 col-lg-3 justify-content-center" style={{ paddingTop:"12px"}}>
+   
+    <div className="card project-card-height" style={{color: "black"}}>
+      
+      <div className="card-body project-card-body">
+        <h5 className="card-title" >{projectList.name}</h5>
+        <p className="card-text">{projectList.description}</p>
+        <br/>
+        <a href={projectList.html_url} target="_blank" className="btn btn-outline-secondary project-card-button ">Check Out</a>
+      </div>
+      </div>
+  </div>
+
+  ))}
+
+</div>
+</div>
+)
+}
+
+
+
 function Projects(){
 
     const [loading, setLoading] = useState(true)
@@ -35,28 +64,7 @@ function Projects(){
       } else if (loading) {
         return <div>Loading...</div>;
       } else {
-        return (
-            <div className="container">
-            <div className="row ">
-          
-            {data.map(data => (
-            <div key={data.id} className="col-sm-12 col-md-4 col-lg-3 justify-content-center" style={{ paddingTop:"12px"}}>
-             
-              <div className="card" style={{color: "black"}}>
-                <img src="..." className="card-img-top" alt="..."/>
-                <div className="card-body">
-                  <h5 className="card-title" >Card title</h5>
-                  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                  <a href="#" className="btn btn-primary">Go somewhere</a>
-                </div>
-                </div>
-            </div>
-
-            ))}
-         
-          </div>
-          </div>
-        );
+        return <ProjectList projectList={data}></ProjectList>
       }
    
 }
