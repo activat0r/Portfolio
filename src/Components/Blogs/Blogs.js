9a5bcb0cbcfcc262b809c8db262383ee79162bf3
Aditya <adisawant100.as@gmail.com>
2021-03-23 14:21:09 +0530

    Added Rainfall wrapper to the blogs page

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -1,10 +1,13 @@
 import BlogsList from './BlogsList'
 import react from 'react'
 import "./../../CSS/Blogs.css"
+import Wrapper from './../../Components/Common/Loading/Wrapper'
 function Blogs(){
     return(
 <div className="container blogsWrapper">
     <h2>In Progress</h2>
+
+    <Wrapper></Wrapper>
 </div>
     )
 }
