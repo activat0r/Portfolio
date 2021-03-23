0f21fb216c135fbc00c1c7e5d5bf43fb7d9fc3ff
Aditya <adisawant100.as@gmail.com>
2021-03-23 14:07:23 +0530

    Added semi transparent background for Blogs and Projects screen

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -11,8 +11,49 @@ import Wrapper from "./../Common/Loading/Wrapper"
 function ProjectList({projectList}){
   
   return(    
-  <div className="container vertical-align">
-  <div className="row hover-animation">
+  <div className="container vertical-align  hover-animation">
+  <div className="row ProjectsRow">
+  <h5 className="projectHeader">Organizational Projects</h5>
+
+   <div className="col-sm-12 col-md-4 justify-content-center  project-card">
+    
+     <div className="card project-card-height card-hovered" style={{color: "black"}}>
+       
+       <div className="card-body project-card-body">
+         <h5 className="card-title" >AgileOnGo</h5>
+         <p className="card-text">Mobile application for showing customized dashboards containing portfolio level metrics and progress indicaters to the stakeholders. The app uses ALM tools like Jira for its data.</p>
+       </div>
+       </div>
+   </div>
+
+   <div className="col-sm-12 col-md-4 justify-content-center  project-card">
+    
+    <div className="card project-card-height card-hovered" style={{color: "black"}}>
+      
+      <div className="card-body project-card-body">
+        <h5 className="card-title" >Agile Maturity Assessment portal</h5>
+        <p className="card-text">A web portal created to check the agile maturity level of the teams and analyse the teams for improvement areas.</p>
+      </div>
+      </div>
+  </div>
+
+  <div className="col-sm-12 col-md-4 justify-content-center  project-card">
+    
+     <div className="card project-card-height card-hovered" style={{color: "black"}}>
+       
+       <div className="card-body project-card-body">
+         <h5 className="card-title" >Release Success Prediction</h5>
+         <p className="card-text">App which uses historical data related to team's velocity to predict whether the release will be successful or not.</p>
+       </div>
+       </div>
+   </div>
+
+  </div>
+
+
+
+  <div className="row ProjectsRow">
+  <h5 className="projectHeader">Other Projects</h5>
 
   {projectList.map(projectList => projectList.fork == false ? (
    
